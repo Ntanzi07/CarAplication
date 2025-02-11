dcc9c3cf0432cc330c9a078058cc220e82022830
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-02-11 17:04:50 -0300

    Creating Footer.tsx and Navbar.tsx, config on the layout.tsx

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import Footer from "./footer";
+import Navbar from "./Navbar";
 
 export {
     Hero,
     CustomButton,
+    Footer,
+    Navbar,
 }
