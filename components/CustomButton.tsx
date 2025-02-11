f9bbfaf7ea356ab615b9519e8ea6c9cf88f469f8
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-02-11 16:58:26 -0300

    Creating image title

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,20 +6,21 @@ import Image from "next/image"
 const CustomButton = ({
     title,
     containerStyles,
-    handleClick    
-}: CustomButtonProps) => {
-  return (
-    <button
-        disabled={false}
-        type={"button"}
-        className={`custom-btn`}
-        onClick={() => {}}
-    >
-        <span className={"flex-1"} >
-            title
-        </span>
-    </button>
-  )
+    handleClick
+}: CustomButtonProps
+) => {
+    return (
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn ${containerStyles}`}
+            onClick={() => { handleClick }}
+        >
+            <span className={"flex-1"} >
+                {title}
+            </span>
+        </button>
+    )
 }
 
 export default CustomButton
