e58964fad5eaaefec29d97754db23b956dab74c7
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-02-17 14:52:56 -0300

    fix the code

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -45,31 +45,22 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             afterLeave={() => setQuery('')}
           >
             <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== ""
-                ? (
-                  <Combobox.Option
-                    value={query}
-                    className="search-manufacturer__option"
-                  >
-                    Create "{query}"
-                  </Combobox.Option>
-                ) : (
-                  filteredManufacturers.map((item) => (
-                    <Combobox.Option
-                      key={item}
-                      className={({ active }) => `
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) => `
                         relative search-manufacturer__option
                         ${active
-                          ? 'bg-primary-blue text-white'
-                          : 'text-gray-900'}
+                      ? 'bg-primary-blue text-white'
+                      : 'text-gray-900'}
                       `}
-                      value={item}
-                    >
-                      {item}
-                    </Combobox.Option>
+                  value={item}
+                >
+                  {item}
+                </Combobox.Option>
 
-                  ))
-                )}
+              ))
+              }
             </Combobox.Options>
           </Transition>
         </div>
